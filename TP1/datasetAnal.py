1f9d7f414e3fd75d2dc03b876ce3bb2808ef7307
rickardo18 <60904677+rickardo18@users.noreply.github.com>
2024-02-10 16:11:50 +0000

    data stored in dictionary

--- TP1/datasetAnal.py
+++ TP1/datasetAnal.py
@@ -4,6 +4,23 @@ import os
 current_directory = os.path.dirname(__file__)
 csv_file_path = os.path.join(current_directory, 'emd.csv')
 
-with open(csv_file_path, 'r') as file:
-    for linha in file:
-        print(linha)
+
+# Define an empty list to store the dictionaries
+data = []
+
+# Open the CSV file and read its contents
+with open('emd.csv', 'r') as file:
+    # Read the header line to get column names
+    headers = file.readline().strip().split(',')
+
+    # Iterate over each line in the file
+    for line in file:
+        # Split the line into values
+        values = line.strip().split(',')
+
+        # Create a dictionary for the current row
+        row_dict = dict(zip(headers, values))
+
+        # Append the row (dictionary) to the data list
+        data.append(row_dict)
+
