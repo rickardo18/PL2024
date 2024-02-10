752dbcde069dc07012fa713bcc07212b6a673674
rickardo18 <60904677+rickardo18@users.noreply.github.com>
2024-02-10 15:53:55 +0000

    read data from file

--- TP1/datasetAnal.py
+++ TP1/datasetAnal.py
@@ -1,4 +1,9 @@
 import sys
+import os
 
-for linha in sys.stdin:
-    print(linha)
+current_directory = os.path.dirname(__file__)
+csv_file_path = os.path.join(current_directory, 'emd.csv')
+
+with open(csv_file_path, 'r') as file:
+    for linha in file:
+        print(linha)
